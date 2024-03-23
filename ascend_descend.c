e186e914ab6cf58580e5c479e8ced2597ef904a6
useranonymous00101 <khatrirohan430@gmail.com>
2024-03-23 09:58:26 +0545

    completed assecnd and descend

--- ascend_descend.c
+++ ascend_descend.c
@@ -1,1 +1,48 @@
-this is c program
+
+#include <stdio.h>
+int main()
+{
+    int i, j, n, temp;
+
+    printf("Enter the no. of numbers: ");
+    scanf("%d", &n);
+    int numbers[n];
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("\nOriginal Array: \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+
+    printf("\n\nArray in Ascending Order: \n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (numbers[i] > numbers[j])
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", numbers[i]);
+    }
+
+    printf("\n\nArray in Descending Order: \n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d\t", numbers[n - i]);
+    }
+
+    return 0;
+}
--- c_program.c
+++ c_program.c
@@ -2,5 +2,6 @@
 #include <conio.h>
 int main()
 {
+
     return 0;
 }
